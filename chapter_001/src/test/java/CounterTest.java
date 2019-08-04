fea024db7c90b9fab7b32a8a2b2978f7f7481948
BischevRamil <bischev@gmail.com>
2019-08-04 16:31:47 +0500

    Подсчет суммы

---
+++ chapter_001/src/test/java/CounterTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import ru.job4j.loop.Counter;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void sum20() {
+        Counter counter=new Counter();
+        int result = counter.add(0,20);
+        assertThat(result, is(110));
+    }
+}
