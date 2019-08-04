80a5be36aa5d111a109c75c420544848b3aee40c
BischevRamil <bischev@gmail.com>
2019-08-04 20:06:06 +0500

    Fitness

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        int i = ivan;
+        int n = nik;
+        while (i<=n) {
+            i*=3;
+            n*=2;
+            month++;
+        }
+        return month;
+    }
+}
---
+++ chapter_001/src/test/java/FitnessTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import ru.job4j.loop.Fitness;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(95, 90);
+        assertThat(days, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(90, 95);
+        assertThat(days, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(50, 90);
+        assertThat(days, is(2));
+    }
+}
