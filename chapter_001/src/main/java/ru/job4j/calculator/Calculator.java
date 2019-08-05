2bcd6d3fcabb7332c01272c15f2c530b77b42894
BischevRamil <bischev@gmail.com>
2019-08-05 15:23:04 +0500

    JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,48 @@
 package ru.job4j.calculator;
 
+/**
+ * Класс для вычисления арифметических операций.
+ * @author Bischev Ramil
+ * @since 05-08-2019
+ * @version 1.0
+ */
+
 public class Calculator {
-    // метод сложения
+    /**
+     * метод сложения
+     * @param first
+     * @param second
+     */
     public static void add(double first, double second) {
         double result=first+second;
         System.out.println(first + "+" + second + " = " + result);
     }
-    // метод вычитания
+
+    /**
+     * метод вычитания
+     * @param first
+     * @param second
+     */
     public static void subtract(double first, double second) {
         double result=first-second;
         System.out.println(first + "-" + second + " = " + result);
     }
-    // метод деления
+
+    /**
+     * метод деления
+     * @param first
+     * @param second
+     */
     public static void div(double first, double second) {
         double result=first/second;
         System.out.println(first + "/" + second + " = " + result);
     }
-    // метод умножения
+
+    /**
+     * метод умножения
+     * @param first
+     * @param second
+     */
     public static void multiple(double first, double second) {
         double result=first*second;
         System.out.println(first + "*" + second + " = " + result);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package_info.java
@@ -0,0 +1,12 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Bischev Ramil
+ * @version $Id$
+ * @since 0.1
+ */
+
+package ru.job4j.calculator;
+
+public class package_info {
+}
