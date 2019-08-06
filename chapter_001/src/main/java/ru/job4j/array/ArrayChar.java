27d45f3a795627055a0f6f523c8455d0fcd360a3
BischevRamil <bischev@gmail.com>
2019-08-06 08:41:42 +0500

    6.4 Слово начинается с...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+/**
+ * Класс проверяет начинается ли слово с данного префикса
+ * @author Bischev Ramil
+ * @since 06-08-2019
+ * @version 1.0
+ */
+
+public class ArrayChar {
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean startsWith(String word, String prefix) {
+        boolean result = true;
+        char[] pref = prefix.toCharArray();
+        char[] wrd = word.toCharArray();
+        // проверить. что массив data имеет первые элементы одинаковые с value
+        for (int i=0; i<pref.length; i++) {
+            if (pref[i]!=wrd[i]){
+                result=false;
+                break;
+            }
+        }
+        return result;
+    }
+}
