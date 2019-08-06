0e321294dc492a2f1132f68d0401ed5f5d033960
BischevRamil <bischev@gmail.com>
2019-08-06 08:32:02 +0500

    6.3 Проверка массива

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * Проверка массива на True или False
+ * @author Bischev Ramil
+ * @since 06-08-2019
+ * @version 1.0
+ */
+
+public class Check {
+
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+       for (int i=0; i<data.length-1; i++){
+           if (data[i] != data[i+1]) {
+               result=false;
+               break;
+           }
+       }
+        return result;
+    }
+}
