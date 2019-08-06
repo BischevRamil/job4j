d69b7c1f54567944cb5c1f0119f4ec68b10cbf9e
BischevRamil <bischev@gmail.com>
2019-08-06 11:09:07 +0500

    6.6 Двухмерный массив. Таблица умножения

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ * Класс составляющий таблицу Пифагора(умножения)
+ * @author Bischev Ramil
+ * @since 06-08-2019
+ * @version 1.0
+ */
+
+public class Matrix {
+
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i<size; i++){
+            for (int j = 0; j<size; j++){
+                table[i][j] = (i+1) * (j+1);
+            }
+        }
+        return table;
+    }
+}
