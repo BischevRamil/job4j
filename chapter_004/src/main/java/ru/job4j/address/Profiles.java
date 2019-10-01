8b35e66eff8a785359e6a0e33476e1e478f4bf36
BischevRamil <bischev@gmail.com>
2019-10-01 22:54:39 +0500

    3. Уникальность элементов и сортировка.[#166304]

--- chapter_004/src/main/java/ru/job4j/address/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/address/Profiles.java
@@ -1,12 +1,18 @@
 package ru.job4j.address;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Profiles {
+
     List<Address> collect(List<Profile> profiles) {
-    return profiles.stream().map(a -> a.getAddress()).collect(Collectors.toList());
+    List<Address> addressList = profiles.stream().map(Profile::getAddress).sorted(new Comparator<Address>() {
+        @Override
+        public int compare(Address address1, Address address2) {
+            return address1.getCity().compareTo(address2.getCity());
+        }
+    }).collect(Collectors.toList());
+        return addressList.stream().distinct().collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/address/ProfileTest.java
+++ chapter_004/src/test/java/ru/job4j/address/ProfileTest.java
@@ -11,13 +11,25 @@ public class ProfileTest {
     Profiles profiles = new Profiles();
     Address adr1 = new Address("msk", "str1", 1, 2);
     Address adr2 = new Address("spb", "str2", 2, 5);
+    Address adr3 = new Address("msk", "str1", 1, 2);
+    Address adr4 = new Address("omsk", "str3", 5, 8);
     Profile profile1 = new Profile(adr1);
     Profile profile2 = new Profile(adr2);
-    List<Profile> profileList = new ArrayList<>(List.of(profile1, profile2));
+    Profile profile3 = new Profile(adr3);
+    Profile profile4 = new Profile(adr4);
+
 
     @Test
-    public void profileTest() {
+    public void addressMapTest() {
+        List<Profile> profileList = new ArrayList<>(List.of(profile1, profile2));
         List<Address> expected = new ArrayList<>(List.of(adr1, adr2));
+        assertThat(expected, is(this.profiles.collect(profileList)));
+    }
+
+    @Test
+    public void addressDistinctTest() {
+        List<Profile> profileList = new ArrayList<>(List.of(profile1, profile2, profile3, profile4));
+        List<Address> expected = new ArrayList<>(List.of(adr1, adr4, adr2));
         assertThat(expected, is(this.profiles.collect(profileList)));
     }
 }
