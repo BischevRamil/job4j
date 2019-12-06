0655c8dc074f2e0907d0aa8604d1b9f6d001c246
BischevRamil <bischev@gmail.com>
2019-12-06 15:36:05 +0500

    1. Читаем файл конфигурации[#196583]

---
+++ Junior/src/main/java/IO/IO/Config.java
@@ -0,0 +1,52 @@
+package IO.IO;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            String delimeter = "=";
+            String[] subStr;
+            String line;
+            while ((line = read.readLine()) != null) {
+                if (!line.isEmpty() && line.charAt(0) != '#') {
+                    subStr = line.split(delimeter);
+                    values.put(subStr[0], subStr[1]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
