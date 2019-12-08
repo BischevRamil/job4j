b735bda414641f1c0d7f2312ecb17c88e82bcd79
BischevRamil <bischev@gmail.com>
2019-12-08 17:32:56 +0500

    1. Читаем файл конфигурации[#196583].v.2

--- Junior/src/main/java/IO/IO/Config.java
+++ Junior/src/main/java/IO/IO/Config.java
@@ -1,11 +1,17 @@
 package IO.IO;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.StringJoiner;
+import java.util.stream.Stream;
 
+/**
+ * @author Bischev Ramil
+ * @since 2019-12-08
+ * Метод load должен загружать пару ключ-значение в Map values.
+ */
 public class Config {
 
     private final String path;
@@ -16,16 +22,12 @@ public class Config {
     }
 
     public void load() {
-        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            String delimeter = "=";
-            String[] subStr;
-            String line;
-            while ((line = read.readLine()) != null) {
-                if (!line.isEmpty() && line.charAt(0) != '#') {
-                    subStr = line.split(delimeter);
-                    values.put(subStr[0], subStr[1]);
-                }
-            }
+        try (Stream<String> stream = Files.lines(Paths.get(this.path))) {
+            stream.filter(i -> i.contains("="))
+            .forEach(i -> {
+                String[] el = i.split("=");
+                values.put(el[0], el[1]);
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
