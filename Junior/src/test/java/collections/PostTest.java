df3a64c7e164b9ee83a429b16e947c973c44e8ea
BischevRamil <bischev@gmail.com>
2020-01-28 18:22:56 +0500

    Почта. [#184458]. add comments.

--- Junior/src/test/java/collections/PostTest.java
+++ Junior/src/test/java/collections/PostTest.java
@@ -1,6 +1,5 @@
 package collections;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
@@ -9,8 +8,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class PostTest {
-    Post post = new Post();
-    private Map<String, HashSet<String>> resultMap = new HashMap<>();
+    private Map<String, HashSet<String>> resultMap;
+    private final String e1 = "xxx@ya.ru";
+    private final String e2 = "foo@gmail.com";
+    private final String e3 = "lol@mail.ru";
+    private final String e4 = "ups@pisem.net";
+    private final String e5 = "xyz@pisem.net";
+    private final String e6 = "vasya@pupkin.com";
+    private final String e7 = "aaa@bbb.ru";
 
     /**
      * На входе:
@@ -24,28 +29,51 @@ public class PostTest {
      * user1 -> xxx@ya.ru,foo@gmail.com,lol@mail.ru,ups@pisem.net,aaa@bbb.ru
      * user5 -> xyz@pisem.net,vasya@pupkin.com
      */
-    @Before
-    public void init() {
-        String e1 = "xxx@ya.ru";
-        String e2 = "foo@gmail.com";
-        String e3 = "lol@mail.ru";
-        String e4 = "ups@pisem.net";
-        String e5 = "xyz@pisem.net";
-        String e6 = "vasya@pupkin.com";
-        String e7 = "aaa@bbb.ru";
 
-        this.post.add("user1", new HashSet<>(Set.of(e1, e2, e3)));
-        this.post.add("user2", new HashSet<>(Set.of(e2, e4)));
-        this.post.add("user3", new HashSet<>(Set.of(e5, e6)));
-        this.post.add("user4", new HashSet<>(Set.of(e4, e7)));
-        this.post.add("user5", new HashSet<>(Set.of(e5)));
+    @Test
+    public void postTest1() {
+        Post post = new Post();
+        this.resultMap = new HashMap<>();
+        post.add("user1", new HashSet<>(Set.of(e1, e2, e3)));
+        post.add("user2", new HashSet<>(Set.of(e2, e4)));
+        post.add("user3", new HashSet<>(Set.of(e5, e6)));
+        post.add("user4", new HashSet<>(Set.of(e4, e7)));
+        post.add("user5", new HashSet<>(Set.of(e5)));
 
         this.resultMap.put("user1", new HashSet<>(Set.of(e1, e2, e3, e4, e7)));
         this.resultMap.put("user5", new HashSet<>(Set.of(e5, e6)));
+        assertThat(post.run(), is(this.resultMap));
     }
 
     @Test
-    public void postTest() {
-        assertThat(this.post.run(), is(this.resultMap));
+    public void postTest2() {
+        Post post = new Post();
+        this.resultMap = new HashMap<>();
+        post.add("user1", new HashSet<>(Set.of(e1)));
+        post.add("user2", new HashSet<>(Set.of(e2)));
+        post.add("user3", new HashSet<>(Set.of(e3)));
+        post.add("user4", new HashSet<>(Set.of(e4)));
+        post.add("user5", new HashSet<>(Set.of(e5)));
+
+        this.resultMap.put("user1", new HashSet<>(Set.of(e1)));
+        this.resultMap.put("user2", new HashSet<>(Set.of(e2)));
+        this.resultMap.put("user3", new HashSet<>(Set.of(e3)));
+        this.resultMap.put("user4", new HashSet<>(Set.of(e4)));
+        this.resultMap.put("user5", new HashSet<>(Set.of(e5)));
+        assertThat(post.run(), is(this.resultMap));
+    }
+
+    @Test
+    public void postTest3() {
+        Post post = new Post();
+        this.resultMap = new HashMap<>();
+        post.add("user1", new HashSet<>(Set.of(e1)));
+        post.add("user2", new HashSet<>(Set.of(e1, e2)));
+        post.add("user3", new HashSet<>(Set.of(e1, e2, e3)));
+        post.add("user4", new HashSet<>(Set.of(e1, e2, e3, e4)));
+        post.add("user5", new HashSet<>(Set.of(e1, e2, e3, e4, e5)));
+
+        this.resultMap.put("user1", new HashSet<>(Set.of(e1, e2, e3, e4, e5)));
+        assertThat(post.run(), is(this.resultMap));
     }
 }
