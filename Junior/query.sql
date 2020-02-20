d4576c8b046724bb296a6241888bf6c747fdbcbc
BischevRamil <bischev@gmail.com>
2020-02-20 22:15:57 +0500

    Фильтры[#196529]

---
+++ Junior/query.sql
@@ -0,0 +1,23 @@
+--Написать запрос получение всех продуктов с типом "СЫР"
+select name from product where type_id=1;
+
+--Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
+select name from product where name='мороженое';
+
+--Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
+select * from product where expired_date>(date_trunc('month', current_date)+interval '1 month') and expired_date<(date_trunc('month', current_date)+interval '2 month');
+
+--Написать запрос, который выводит самый дорогой продукт.
+select * from product order by price desc limit 1;
+
+--Написать запрос, который выводит количество всех продуктов определенного типа.
+select t.name, count(*) from type t left join product p on t.id=p.type_id group by t.name;
+
+--Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
+select * from product where type_id=1 or type_id=2;
+
+--Написать запрос, который выводит тип продуктов, которых осталось меньше 4 штук.
+select t.name, count(*) from type t left join product p on t.id=p.type_id group by t.name having count(*)<4;
+
+--Вывести все продукты и их тип.
+select p.name, t.name from product p left join type t on t.id=p.type_id;
