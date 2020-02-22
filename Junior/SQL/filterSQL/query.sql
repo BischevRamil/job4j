a03ee6165aceba2d25e9ecfabdcfc298d148c165
BischevRamil <bischev@gmail.com>
2020-02-22 21:03:47 +0500

    Фильтры[#196529]

--- Junior/SQL/filterSQL/query.sql
+++ Junior/SQL/filterSQL/query.sql
@@ -1,5 +1,5 @@
 --Написать запрос получение всех продуктов с типом "СЫР"
-select name from product where type_id=1;
+select p.name, t.name from product p left join type t on p.type_id=t.id where t.name like 'сыр';
 
 --Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
 select name from product where name='мороженое';
@@ -14,7 +14,8 @@ select * from product order by price desc limit 1;
 select t.name, count(*) from type t left join product p on t.id=p.type_id group by t.name;
 
 --Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
-select * from product where type_id=1 or type_id=2;
+select p.name, t.name from product p left join type t on p.type_id=t.id where t.name like 'сыр' or t.name like 'молоко';
+
 
 --Написать запрос, который выводит тип продуктов, которых осталось меньше 4 штук.
 select t.name, count(*) from type t left join product p on t.id=p.type_id group by t.name having count(*)<4;
