092ac632a4f2cfd057b37405d2cb1189507553c2
BischevRamil <bischev@gmail.com>
2020-02-28 18:33:08 +0500

    4.3. XML XSLT JDBC Оптимизация[#196517]

---
+++ Junior/src/main/java/sql/magnit/Entry.java
@@ -0,0 +1,19 @@
+package sql.magnit;
+
+import javax.xml.bind.annotation.*;
+@XmlRootElement(name = "entry")
+@XmlAccessorType (XmlAccessType.FIELD)
+public class Entry {
+    private int entry;
+
+    public Entry() {
+    }
+
+    public int getEntry() {
+        return entry;
+    }
+
+    public void setEntry(int entry) {
+        this.entry = entry;
+    }
+}
---
+++ Junior/src/main/java/sql/magnit/StoreSQL.java
@@ -0,0 +1,111 @@
+package sql.magnit;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Ramil Bischev
+ * @since 2020-02-27
+ * Класс работы с базой данных
+ */
+public class StoreSQL implements AutoCloseable {
+    private final Config config;
+    private Connection connect;
+    private static final Random RN = new Random();
+
+    public StoreSQL(Config config) {
+        this.config = config;
+    }
+
+    /**
+     * Соединение с базой данных создание таблицы entries.
+     *
+     * @return true если соединение успешно.
+     */
+    public boolean connectToDB() {
+        this.config.init();
+        try {
+            this.connect = DriverManager.getConnection(this.config.get("url"));
+            Statement st = connect.createStatement();
+            st.execute("create table if not exists entries (entry integer);");
+            this.clearTable("entries");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return this.connect != null;
+    }
+
+    /**
+     * Генерируется size целочисленных значений и вносятся в БД.
+     * @param size количество значений.
+     */
+    public void generate(int size) {
+        if (connectToDB()) {
+            for (int i = 1; i <= size; i++) {
+                Entry entry = new Entry();
+                entry.setEntry(generateInt());
+                try (PreparedStatement statement = connect.prepareStatement(SQLItems.INSERT.QUERY)) {
+                    statement.setInt(1, entry.getEntry());
+                    statement.execute();
+                }
+                 catch (Exception e) {
+                     System.out.println(e.getMessage());
+                 }
+            }
+        }
+    }
+
+    /**
+     * Выгрузка значений из БД в List.
+     * @return List.
+     */
+    public List<Entry> load() {
+        List<Entry> listResult = new ArrayList<>();
+        try (PreparedStatement st = connect.prepareStatement(SQLItems.GETALL.QUERY)) {
+            ResultSet resultSet = st.executeQuery();
+            while (resultSet.next()) {
+                Entry entry = new Entry();
+                entry.setEntry(resultSet.getInt("entry"));
+                listResult.add(entry);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return listResult;
+    }
+
+    @Override
+    public void close() throws Exception {
+        connect.close();
+    }
+
+     /** Удаление таблицы
+     * @param tableName имя таблицы
+      */
+    public void clearTable(String tableName) {
+        try (Statement statement = connect.createStatement()) {
+            statement.execute("DELETE FROM " + tableName + ";");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    enum SQLItems {
+        GETALL("SELECT * FROM entries;"),
+        INSERT("INSERT INTO entries (entry) VALUES (?);");
+
+        String QUERY;
+
+        SQLItems(String QUERY) {
+            this.QUERY = QUERY;
+        }
+    }
+
+    private int generateInt() {
+        return RN.nextInt(100);
+    }
+}
