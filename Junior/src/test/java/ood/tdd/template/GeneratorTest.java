752bc454a990c66d95d078919a30478faa4fbbf0
Ramil Bischev <bischev@gmail.com>
2020-05-11 19:40:35 +0500

    1. Исправить код класс SimpleGenerator. [#196618]

---
+++ Junior/src/test/java/ood/tdd/template/GeneratorTest.java
@@ -0,0 +1,59 @@
+package ood.tdd.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenInsertNameAndSubjectThanReturnResult() throws Exception {
+        Generator generator = new SimpleGenerator();
+        String text = "I am ${name}, Who are ${subject}?";
+        Map<String, String> inputMap = new HashMap<>();
+        inputMap.put("name", "Petr");
+        inputMap.put("subject", "you");
+        String expected = "I am Petr, Who are you?";
+        String result = generator.produce(text, inputMap);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenInsertOneNameThenReturnName() throws Exception {
+        Generator generator = new SimpleGenerator();
+        String text = "I am ${name}, Who are you?";
+        Map<String, String> inputMap = new HashMap<>();
+        inputMap.put("name", "Petr");
+        String expected = "I am Petr, Who are you?";
+        String result = generator.produce(text, inputMap);
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = Exception.class)
+    public void whenAddNotEnoughKeysThenException() throws Exception {
+        Generator generator = new SimpleGenerator();
+        String text = "I am ${name}, Who are you?";
+        Map<String, String> inputMap = new HashMap<>();
+        inputMap.put("name", "Petr");
+        inputMap.put("subject", "you");
+        String expected = "I am Petr, Who are you?";
+        String result = generator.produce(text, inputMap);
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = Exception.class)
+    public void whenAddToManyKeysThenException() throws Exception {
+        Generator generator = new SimpleGenerator();
+        String text = "I am ${name}, Who are ${subject}? Where are ${object}?";
+        Map<String, String> inputMap = new HashMap<>();
+        inputMap.put("name", "Petr");
+        inputMap.put("subject", "you");
+        String expected = "I am Petr, Who are you?";
+        String result = generator.produce(text, inputMap);
+        assertThat(result, is(expected));
+    }
+}
