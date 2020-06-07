9ca6a06fc4781938565604eef6410b0324a76f2f
Ramil Bischev <bischev@gmail.com>
2020-06-07 15:05:13 +0500

    Режим ожидания. [#302726]

---
+++ middle/src/main/java/multithreading/Wget.java
@@ -0,0 +1,28 @@
+package multithreading;
+
+/**
+ * @author Bischev Ramil
+ * @since 2020-06-07
+ * Class imitate load process.
+ */
+public class Wget {
+
+    public static void main(String[] args) {
+        Thread load = new Thread(
+                () -> {
+                    for (int i = 0; i <= 100; i++) {
+                        System.out.print("\rLoading : " + i  + "%");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException ie) {
+                            ie.printStackTrace();
+                        }
+
+                    }
+
+                }
+        );
+
+        load.start();
+    }
+}
