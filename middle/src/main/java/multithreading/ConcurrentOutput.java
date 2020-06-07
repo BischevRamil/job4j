9bb70228c346eacdf09fef724be43c47a4889fcf
Ramil Bischev <bischev@gmail.com>
2020-06-07 13:52:51 +0500

    Запуск нити. Thread#start() [#302728]

---
+++ middle/src/main/java/multithreading/ConcurrentOutput.java
@@ -0,0 +1,20 @@
+package multithreading;
+
+/**
+ * @author Bischev Ramil
+ * @since 2020-06-07
+ * Multi-thread example
+ */
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
