074ec2796ec85148cc72a6c09fd68f28483ad104
Ramil Bischev <bischev@gmail.com>
2020-06-07 16:01:02 +0500

    Прерывание нити [#302729]

---
+++ middle/src/main/java/multithreading/ConsoleProgressRun.java
@@ -0,0 +1,45 @@
+package multithreading;
+
+/**
+ * @author Bischev Ramil
+ * @since 2020-06-07
+ * Interrupt the thread.
+ */
+class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var i = 0;
+        String s = "";
+        while (!Thread.currentThread().isInterrupted()) {
+            i++;
+            switch (i % 3) {
+                case 0: s = "\\";
+                break;
+                case 1: s = "|";
+                break;
+                case 2: s = "/";
+                break;
+            }
+
+            System.out.print("\r load: " + s);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
+
+public class ConsoleProgressRun {
+    public static void main(String[] args) {
+        Thread run = new Thread(new ConsoleProgress());
+        run.start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        run.interrupt();
+    }
+}
