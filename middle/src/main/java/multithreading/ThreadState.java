ee1de66e6eee8ce3f35662b92f96e83c149faaa2
Ramil Bischev <bischev@gmail.com>
2020-06-08 22:19:33 +0500

    Состояние нити. [#302725] refactor

--- middle/src/main/java/multithreading/ThreadState.java
+++ middle/src/main/java/multithreading/ThreadState.java
@@ -10,18 +10,16 @@ public class ThreadState {
         Thread first = new Thread(
                 () -> System.out.println("First thread")
         );
-
         Thread second = new Thread(
                 () -> System.out.println("Second thread")
         );
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println("First thread state is " + first.getState());
             System.out.println("Second thread state is " + second.getState());
             System.out.println("wait");
         }
-
         System.out.println("First thread state is " + first.getState());
         System.out.println("Second thread state is " + second.getState());
         System.out.println("Threads is terminated");
