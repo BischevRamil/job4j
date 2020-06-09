98d69c994e62d404a3823d73e5d03b91ac3fcc6b
Ramil Bischev <bischev@gmail.com>
2020-06-10 01:15:45 +0500

    Синхронизация общих ресурсов. [#302735]

---
+++ middle/src/main/java/multithreading/Cache.java
@@ -0,0 +1,12 @@
+package multithreading;
+
+public class Cache {
+    private static Cache cache;
+
+    private synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
