addf681c6e7f6ec2351d482698bfa7319ca0d319
Ramil Bischev <bischev@gmail.com>
2020-06-10 12:15:57 +0500

    Модель памяти Java [#302732]

---
+++ middle/src/main/java/multithreading/DCLSingleton.java
@@ -0,0 +1,19 @@
+package multithreading;
+
+public class DCLSingleton {
+    private volatile static DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+}
