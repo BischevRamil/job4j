e9d4abce719962e6fb21e6ab0978d8e35af7dd92
Ramil Bischev <bischev@gmail.com>
2020-06-14 23:12:19 +0500

    Управление нитью через wait. [#302722]

---
+++ middle/src/main/java/multithreading/CountBarrier.java
@@ -0,0 +1,27 @@
+package multithreading;
+
+public class CountBarrier {
+    private final Object monitor = this;
+    private final int total;
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public synchronized void count() {
+        this.count++;
+        monitor.notifyAll();
+    }
+
+    public synchronized void await() {
+        while (total != count) {
+            try {
+                monitor.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
+}
---
+++ middle/src/main/java/multithreading/CountBarrierUse.java
@@ -0,0 +1,30 @@
+package multithreading;
+
+public class CountBarrierUse {
+
+    public static void main(String[] args) {
+        CountBarrier countBarrier = new CountBarrier(100);
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " started");
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(i);
+                countBarrier.count();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            countBarrier.await();
+            System.out.println(Thread.currentThread().getName() + " started");
+
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
