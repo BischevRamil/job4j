ce4c1313f7442117940174d4b681daae12b72919
Ramil Bischev <bischev@gmail.com>
2020-06-16 23:35:58 +0500

    Обеспечить остановку потребителя. [#302719]

---
+++ middle/src/main/java/multithreading/ParallelSearch.java
@@ -0,0 +1,37 @@
+package multithreading;
+
+
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+    }
+}
