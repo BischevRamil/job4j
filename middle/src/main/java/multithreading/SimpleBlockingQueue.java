0a275aa6e1ddae52d59faa2685cdcf5d259ab0f5
Ramil Bischev <bischev@gmail.com>
2020-06-16 23:19:02 +0500

    Реализовать шаблон Producer Consumer. [#302721]

---
+++ middle/src/main/java/multithreading/SimpleBlockingQueue.java
@@ -0,0 +1,101 @@
+package multithreading;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Bischev Ramil
+ * @since 2020-06-16
+ * Class implements simple bounded blocking queue.
+ * @param <T>
+ */
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private final int size;
+
+    public SimpleBlockingQueue(int size) {
+        this.size = size;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (queue.size() >= this.size) {
+            wait();
+        }
+        queue.add(value);
+        notify();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        T element = queue.poll();
+        notify();
+        return element;
+    }
+
+    public synchronized boolean isEmpty() {
+        return this.queue.isEmpty();
+    }
+}
+
+/**
+ * Class Producer puts elements to queue.
+ * @param <T>
+ */
+class Producer<T> implements Runnable {
+    private SimpleBlockingQueue<T> queue;
+    private List<T> list;
+
+    Producer(SimpleBlockingQueue<T> queue, List<T> list) {
+        this.queue = queue;
+        this.list = list;
+    }
+
+    public void run() {
+        for (T t : list) {
+            System.out.println("Producer try insert data to queue " + t);
+            try {
+                queue.offer(t);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
+
+/**
+ * Class Consumer polls elements from queue.
+ * @param <T>
+ */
+class Consumer<T> implements Runnable {
+    private SimpleBlockingQueue<T> queue;
+    private List<T> list;
+
+    Consumer(SimpleBlockingQueue<T> queue, List<T> list) {
+        this.queue = queue;
+        this.list = list;
+    }
+
+    public void run() {
+        T element;
+        while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+            try {
+                element = queue.poll();
+                System.out.println("Consumer try to poll data " + element);
+                list.add(element);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+
+        }
+    }
+}
---
+++ middle/src/test/java/multithreading/SimpleBlockingQueueTest.java
@@ -0,0 +1,29 @@
+package multithreading;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        List<Integer> listIn = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+        List<Integer> listOut = new ArrayList<>();
+        Producer<Integer> producer = new Producer<>(queue, listIn);
+        Consumer<Integer> consumer = new Consumer<>(queue, listOut);
+        Thread prod = new Thread(producer);
+        prod.start();
+        Thread cons = new Thread(consumer);
+        cons.start();
+        prod.join();
+        cons.interrupt();
+        cons.join();
+        assertThat(listOut, is(listIn));
+    }
+}
