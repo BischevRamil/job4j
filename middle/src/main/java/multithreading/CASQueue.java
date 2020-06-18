e16465b9e64d21bc6dd6233b022a703f73661de4
Ramil Bischev <bischev@gmail.com>
2020-06-18 11:38:56 +0500

    CAS - операции [#302747]

---
+++ middle/src/main/java/multithreading/CASQueue.java
@@ -0,0 +1,56 @@
+package multithreading;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author Bischev Ramil
+ * @since 2020-06-18
+ * CASQueue class implements non-blocking queue.
+ * @param <T>
+ */
+@ThreadSafe
+public class CASQueue<T> {
+    private AtomicReference<Node<T>> head = new AtomicReference<>();
+    private AtomicReference<Node<T>> tail = new AtomicReference<>();
+
+    public void push(T value) {
+        Node<T> temp = new Node<>(value);
+        Node<T> ref;
+        do {
+            ref = tail.get();
+            if (ref == null) {
+                head.set(temp);
+                tail.set(temp);
+                temp.next = null;
+                return;
+            }
+            tail.get().next = temp;
+            tail.set(temp);
+            temp.next = null;
+        } while (!tail.compareAndSet(ref, temp));
+    }
+
+    public T poll() {
+        Node<T> temp;
+        Node<T> ref;
+        do {
+            ref = head.get();
+            if (ref == null) {
+                throw new IllegalStateException("Queue is empty");
+            }
+            temp = ref.next;
+        } while (!head.compareAndSet(ref, temp));
+        return ref.value;
+    }
+
+    private static final class Node<T> {
+        final T value;
+        Node<T> next;
+
+        public Node(final T value) {
+            this.value = value;
+        }
+    }
+}
---
+++ middle/src/test/java/multithreading/CASQueueTest.java
@@ -0,0 +1,58 @@
+package multithreading;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+public class CASQueueTest {
+    @Test
+    public void when1PushThen1Poll() {
+        CASQueue<Integer> queue = new CASQueue<>();
+        queue.push(1);
+        assertThat(queue.poll(), is(1));
+    }
+
+    @Test
+    public void when2PushThen2Poll() {
+        CASQueue<Integer> queue = new CASQueue<>();
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        assertThat(queue.poll(), is(1));
+        assertThat(queue.poll(), is(2));
+        assertThat(queue.poll(), is(3));
+        assertThat(queue.poll(), is(4));
+    }
+
+    @Test
+    public void when3PushAndPollByTwoThreadsThen1234() throws InterruptedException {
+        CASQueue<Integer> queue = new CASQueue<>();
+        List<Integer> result = new ArrayList<>();
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        Thread first = new Thread(() -> {
+            for (int i = 0; i < 2; i++) {
+                result.add(queue.poll());
+            }
+        });
+        Thread second = new Thread(() -> {
+            for (int i = 0; i < 2; i++) {
+                result.add(queue.poll());
+            }
+        });
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertThat(result, is(expected));
+    }
+
+}
