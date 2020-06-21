40fd4dd32af9d17cf356f9d79ebf17c6d90813eb
Ramil Bischev <bischev@gmail.com>
2020-06-21 12:16:36 +0500

    Immutable объекты. [#302733] fix

--- middle/src/main/java/multithreading/sharedresources/UserCache.java
+++ middle/src/main/java/multithreading/sharedresources/UserCache.java
@@ -1,12 +1,13 @@
 package multithreading.sharedresources;
 
-import net.jcip.annotations.NotThreadSafe;
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@NotThreadSafe
+@ThreadSafe
 public class UserCache {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger();
