1ec280f1834976fcf15133baf5cbc33ff5ec9674
Ramil Bischev <bischev@gmail.com>
2020-07-01 11:35:30 +0500

    Thread switcher [#302750] fix

--- middle/src/main/java/multithreading/threads/Switcher.java
+++ middle/src/main/java/multithreading/threads/Switcher.java
@@ -7,74 +7,73 @@ package multithreading.threads;
  * Нить А всегда печатает первой.
  */
 public class Switcher {
-    private volatile boolean aBoolean = true;
-
     public static void main(String[] args) throws InterruptedException {
-        new Switcher().startSwitcher();
-    }
-
-    private void startSwitcher() throws InterruptedException {
-        Thread first = new Thread(new A());
-        Thread second = new Thread(new B());
+        MasterSlaveBarrier masterSlaveBarrier = new MasterSlaveBarrier();
+        Thread first = new Thread(
+                () -> {
+                    while (true) {
+                        masterSlaveBarrier.tryMaster();
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        masterSlaveBarrier.doneMaster();
+                    }
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    while (true) {
+                        masterSlaveBarrier.trySlave();
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        masterSlaveBarrier.doneSlave();
+                    }
+                }
+        );
         first.start();
         second.start();
         first.join();
         second.join();
     }
 
-    private synchronized void printA() {
-        while (!aBoolean) {
-            try {
-                wait();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("Thread A");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        aBoolean = false;
-        notify();
-    }
+    private static class MasterSlaveBarrier {
+        private Boolean isMaster = true;
 
-    private synchronized void printB() {
-        while (aBoolean) {
-            try {
-                wait();
+        public synchronized void tryMaster() {
+            while (!isMaster) {
+                try {
+                    wait();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println("Thread A");
         }
-        System.out.println("Thread B");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        aBoolean = true;
-        notify();
-    }
 
-    private class A implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
-                printA();
+        public synchronized void trySlave() {
+            while (isMaster) {
+                try {
+                    wait();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+            System.out.println("Thread B");
         }
-    }
 
-    private class B implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
-                printB();
-            }
+        public synchronized void doneMaster() {
+            isMaster = false;
+            notify();
+        }
+
+        public synchronized void doneSlave() {
+            isMaster = true;
+            notify();
         }
     }
 }
